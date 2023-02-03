a2b55deb5a9a44bf17f7599bb06638d5458c8e64
chidigit config --global user.name chidi <chidimathayas@gmail.com>
2023-02-03 21:23:43 +0100

    Tenth Commit

--- js/havtent.js
+++ js/havtent.js
@@ -4,41 +4,61 @@ const navBar = document.querySelector('.nav-bar');
 const prevBtn = document.getElementById('prev-btn');
 const nextBtn = document.getElementById('next-btn');
 
-const image_Slide = document.querySelector('.imageSlide');
+const slider = document.querySelector('#image');
+const imageContainer = document.querySelector('.container');
 
 // Nav-bar toggle
 menuIcon.addEventListener('click', () => {
     navBar.classList.toggle('active');
 })
 
-// Image slider and button functionality
-const images = ['image9', 'image2', 'image4']
+// Image slider 
+const images = ['image9', 'image4', 'image2']
 let currentImg = 0;
-// let image = images[currentImg];
+let timeOut;
+
+function updateImg() {
+    const image = images[currentImg]
+    slider.src = `images/${image}.jpeg`;
+    currentImg++
+    
+    if (currentImg == images.length) {
+        currentImg = 0
+    }
+    imageContainer.style.transform = `translateX(-${(currentImg - 1) * 0}px)`;
+    setTimeout(()=> {
+        updateImg()
+    }, 3000)
+}
+updateImg()
+
+
 
 // prevBtn.addEventListener('click', () =>{
-//     currentImg--;
-//     updateImg()
+//     currentImg--
+//     if (currentImg < images.length) {
+//         currentImg = 0
+//     }
+//     updateImg(currentImg)
 // })
 
 // nextBtn.addEventListener('click', () =>{
 //     currentImg++;
-//     updateImg()
-// });
+//     if (currentImg > images.length) {
+//         currentImg = 0
+//     }
+//     updateImg(currentImg)
+// })
 
-function updateImg() {
-    const image = images[currentImg];
-    image_Slide.src = `images/${image}.jpeg`
+// function updateImg() {
+//     const image = images[currentImg]
+//     image_Slide.src = `images/${image}.jpeg`
+//     currentImg++
 
-    if (currentImg == images.length) {
-        currentImg = 0
-    }
-    setTimeout(()=> {
-        updateImg()
-    }, 500)
-    // if (currentImg < 1) {
-    //     currentImg = images.length
-    // } else if (currentImg > images.length) {
-    //     currentImg = 0
-    // }
-}
+//     if (currentImg == images.length) {
+//         imageIndex = 0
+//     }
+//     setTimeout(()=>{
+//         updateImg()
+//     }, 3000)
+// }
