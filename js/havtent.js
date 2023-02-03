153c115d35cd848328174b9bb0fb26de444f642b
chidigit config --global user.name chidi <chidimathayas@gmail.com>
2023-02-04 00:02:48 +0100

    Eleventh Commit

--- js/havtent.js
+++ js/havtent.js
@@ -5,7 +5,7 @@ const prevBtn = document.getElementById('prev-btn');
 const nextBtn = document.getElementById('next-btn');
 
 const slider = document.querySelector('#image');
-const imageContainer = document.querySelector('.container');
+const imageContainer = document.querySelector('.image-container');
 
 // Nav-bar toggle
 menuIcon.addEventListener('click', () => {
@@ -17,6 +17,20 @@ const images = ['image9', 'image4', 'image2']
 let currentImg = 0;
 let timeOut;
 
+prevBtn.addEventListener('click', () =>{
+    if (currentImg < 1) {
+        currentImg = images.length
+    } 
+    updateImg()
+})
+
+nextBtn.addEventListener('click', () =>{
+    if (currentImg > images.length) {
+        currentImg = 0
+    }
+    updateImg()
+});
+
 function updateImg() {
     const image = images[currentImg]
     slider.src = `images/${image}.jpeg`;
@@ -24,41 +38,41 @@ function updateImg() {
     
     if (currentImg == images.length) {
         currentImg = 0
-    }
-    imageContainer.style.transform = `translateX(-${(currentImg - 1) * 0}px)`;
+    } 
+    // imageContainer.style.transform = `translateX(-${(currentImg - 1) * 0}px)`;
     setTimeout(()=> {
         updateImg()
     }, 3000)
 }
 updateImg()
 
-
-
-// prevBtn.addEventListener('click', () =>{
-//     currentImg--
-//     if (currentImg < images.length) {
-//         currentImg = 0
-//     }
-//     updateImg(currentImg)
+// nextBtn.addEventListener("click", ()=>{
+//     currentImg++
+//     clearTimeout(timeOut)
+//     updateImg()
 // })
 
-// nextBtn.addEventListener('click', () =>{
-//     currentImg++;
-//     if (currentImg > images.length) {
-//         currentImg = 0
-//     }
-//     updateImg(currentImg)
+// prevBtn.addEventListener("click", ()=>{
+//     currentImg--
+//     clearTimeout(timeOut)
+//     updateImg()
 // })
 
 // function updateImg() {
 //     const image = images[currentImg]
-//     image_Slide.src = `images/${image}.jpeg`
+//     slider.src = `images/${image}.jpeg`
 //     currentImg++
 
-//     if (currentImg == images.length) {
-//         imageIndex = 0
+//     if (currentImg > images.length) {
+//         currentImg = 1;
+//     }else if(currentImg < 1) {
+//         currentImg = images.length
 //     }
-//     setTimeout(()=>{
+//     // imageContainer.style.transform = `translateX(-${(currentImg - 1) * 1535}px)`;
+//     timeOut = setTimeout(()=>{
+//         currentImg++
 //         updateImg()
 //     }, 3000)
 // }
+
+// updateImg()
