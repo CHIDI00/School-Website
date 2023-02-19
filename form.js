30a86a4e8625b43eebb79107db6c21706927f7b0
chidigit config --global user.name chidi <chidimathayas@gmail.com>
2023-02-20 00:34:39 +0100

    Twenty three commit

---
+++ form.js
@@ -0,0 +1,22 @@
+const signupBtn = document.getElementById('signupBtn');
+const signinBtn = document.getElementById('signinBtn');
+const nameField = document.getElementById('nameField');
+const title = document.getElementById('title');
+
+signinBtn.onclick = () => {
+    nameField.style.maxHeight = '0'
+    signinBtn.style.backgroundColor = '#0a6b40'
+    signinBtn.style.color = '#FFF'
+    signupBtn.style.backgroundColor = '#cbf0bb'
+    signupBtn.style.color = '#0a6b40'
+    // console.log('clicked');
+}
+
+signupBtn.onclick = () => {
+    nameField.style.maxHeight = '65px'
+
+    signupBtn.style.backgroundColor = '#0a6b40'
+    signupBtn.style.color = '#FFF'
+    signinBtn.style.backgroundColor = '#cbf0bb'
+    signinBtn.style.color = '#0a6b40'
+}
