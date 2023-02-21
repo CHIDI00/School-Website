1d0c9e728434fd26c9f10304d59c5599f1626945
chidigit config --global user.name chidi <chidimathayas@gmail.com>
2023-02-21 19:10:58 +0100

    JavaScripttt

--- js/form.js
+++ js/form.js
@@ -7,6 +7,9 @@ const username = document.querySelector('.username').value;
 const email = document.querySelector('.email').value;
 const password = document.querySelector('.password').value;
 
+const container = document.querySelector('.body-container')
+const form_Container = document.querySelector('.container')
+
 
 signinBtn.onclick = () => {
     nameField.style.maxHeight = '0'
@@ -25,8 +28,118 @@ signupBtn.onclick = () => {
     signupBtn.style.color = '#FFF'
     signinBtn.style.backgroundColor = '#cbf0bb'
     signinBtn.style.color = '#0a6b40'
-    console.log('clicked');
+    // console.log('clicked');
+
+    form_Container.style.display = 'none'
+    container.style.display = 'flex'
+    if (username.value == "") {
+        alert("Please enter a username")
+    } else if (email.value == "") {
+        alert("Please enter an email")
+    } else if (password.value == "") {
+        alert("Please enter a password")
+    } else {
+        let profile = document.createElement("div");
+        profile.classList.add("body-container")
+        profile.innerHTML = `
+        <div class="profile-container">
+            <div class="container">
+                <h1>HavTont</h1>
+                <div class="course-list">
+                    <a href="">Python</a>
+                    <a href="">Data Analyst</a>
+                    <a href="">Networking</a>
+                    <a href="">Cloud Computing</a>
+                    <a href="">AWS</a>
+                    <a href="">Python</a>
+                    <a href="">Java</a>
+                    <a href="">JavaScript</a>
+                    <a href="">Ruby</a>
+                </div>
+
+                <!-- <div class="logOut"> -->
+                    <button>Log out<i class="fa-solid fa-right-from-bracket"></i></button>
+                <!-- </div> -->
+            </div>
+        </div>
+
+        <div class="article">
+            <div class="article-container">
+
+                <div class="user-dashboard">
+
+                    <div class="profile">
+                        <img src="images/profilepic.jpg" alt="">
+                        <div class="profile-name">
+                            <h3>${username.value}</h3>
+                            <p>Computer Science</p>
+                        </div>
+                    </div>
+
+                    <div class="profile-board">
+                        <div class="profie-board-2">
+                            <div class="dash-board">
+                                <i class="fa-solid fa-clipboard"></i>
+                                <p>Dash Board</p>
+                            </div>
+                            <div class="dash-board">
+                                <i class="fa-solid fa-user"></i>
+                                <p>Profile</p>
+                            </div>
+                            <div class="dash-board">
+                                <i class="fa-solid fa-book"></i>
+                                <p>Courses</p>
+                            </div>
+                            <div class="dash-board">
+                                <i class="fa-solid fa-user"></i>
+                                <p>Dash Board</p>
+                            </div>
+                            <div class="dash-board">
+                                <i class="fa-solid fa-user"></i>
+                                <p>Dash Board</p>
+                            </div>
+                            <div class="dash-board">
+                                <i class="fa-solid fa-user"></i>
+                                <p>Dash Board</p>
+                            </div>
+                            <div class="dash-board">
+                                <i class="fa-solid fa-user"></i>
+                                <p>Dash Board</p>
+                            </div>
+                            <div class="dash-board">
+                                <i class="fa-solid fa-user"></i>
+                                <p>Dash Board</p>
+                            </div>
+                            <div class="dash-board">
+                                <i class="fa-solid fa-user"></i>
+                                <p>Dash Board</p>
+                            </div>
+                            <div class="dash-board">
+                                <i class="fa-solid fa-user"></i>
+                                <p>Dash Board</p>
+                            </div>
+                            <div class="dash-board">
+                                <i class="fa-solid fa-user"></i>
+                                <p>Dash Board</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="right-board">
+                    <div class="right-board-container">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    </div>
+        `
+        container.appendChild(profile)
+    }
 }
+
 
 
 // signupBtn.onclick = () => {
